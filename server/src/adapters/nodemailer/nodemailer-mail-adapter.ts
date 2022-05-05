13bd847e6b699912aaa05dd8e9d97677b6ba7755
Luan <luanoliveira17@hotmail.com>
2022-05-05 19:47:04 -0300

    Aula 03 - API

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "a3a29e29592233",
+    pass: "409b8b66e010f6"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Luan Oliveira <luanoliveiraltda@gmail.com>',
+      subject,
+      html: body
+    })
+  }
+}
